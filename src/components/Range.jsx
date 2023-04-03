6ae96703db0be24470a66879b1cbfdca09cb40db
N-eeraj <neeraj7rajeena@gmail.com>
2023-04-03 13:18:48 +0530

    Display selected password length

--- src/components/Range.jsx
+++ src/components/Range.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react'
+
 const Button = ({ updateLength }) => {
+    const [passwordLength, setPasswordLength] = useState(24)
+
+    const handleUpdate = ({target}) => {
+        setPasswordLength(target.value)
+        updateLength(target.value)
+    }
+
     return (
+        <div className='range-container'>
+            <span>
+                Password Length:
+                {passwordLength}
+            </span>
         <input
             type="range"
             min={6}
             max={24}
-            onChange={({target}) => updateLength(target.value)} />
+                className='range'
+                onChange={handleUpdate} />
+        </div>
     )
 }
 
